42253165fa13f66b2015702d6ac394a6eded8d1c
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:25:41 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,9 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "spaces" {
+  description = "The spaces created by this component"
+  value       = local.enabled ? local.spaces : {}
+}
+
+output "policies" {
+  description = "The policies created by this component"
+  value       = local.enabled ? local.policies : {}
 }
